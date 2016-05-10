51b1d66febce73753c36bbafd3a00edd5362b920
SilenceFool <martinsoderlund10@gmail.com>
2016-05-10 13:24:01 +0200

    blabla

--- src/test/test.java
+++ src/test/test.java
@@ -10,6 +10,7 @@ public class test {
 		
 		ArrayList<Integer> userInput = new ArrayList<>();
 		System.out.println("Mata in 3x3 siffror:");
+		System.out.println("Bög");
 		Scanner kbd = new Scanner(System.in);
 		for (int i = 0; i < 9; i++) {
 			userInput.add(kbd.nextInt());
