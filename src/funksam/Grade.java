312f20a4923ba95bdfdcb5a1152b5491dd5c0e2d
SilenceFool <martinsoderlund10@gmail.com>
2016-05-11 15:31:50 +0200

    kukbög

--- src/funksam/Grade.java
+++ src/funksam/Grade.java
@@ -1,29 +1,14 @@
 package funksam;
 
-import java.util.ArrayList;
-
 
 public class Grade {
-	
-	public void getGrade(String student) {
-	
-	//Skapa array
-	String[] grades;
-	
-	//Sätter storlek på Arrayen
-	grades = new String[3];
-	
-	//Initiera elementen
-	grades[0] = "U";
-	grades[1] = "G";
-	grades[2] = "VG";
 
-	}
+	//Skapa array och storlek på Arrayen fitta
+	private String[] grades = {"U","G","VG"};
+
 	
 	private String getGrade(int grade) {
 		return this.grades[grade];
-		
-		//apa
-		
 	}
 }
+
