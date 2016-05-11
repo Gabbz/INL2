0aea6fb1481ab95cf82133cb94f7ae181b95b629
sickbruv <tomas.somi@live.se>
2016-05-11 15:58:41 +0200

    Gjort ändringar i klassen course

--- src/funksam/Course.java
+++ src/funksam/Course.java
@@ -1,5 +1,21 @@
+
 package funksam;
+import java.util.ArrayList;
 
 public class Course {
+		
+	public static void main (String[] args) {				
+		
+		ArrayList<String> courseList = new ArrayList<>();
+		
+		String course1 = "725G31";
+		
+		courseList.add(course1);
+		
+		System.out.println(course1);
 
+		//I have no idea what I'm doing.
+		
+	}
+	
 }
